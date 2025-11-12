c9c7558f90353d69d48b8eac9e31d101fbc1870a
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-11-12 16:05:51 -0300

    Habilitando o cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // habilita CORS globalmente
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
