bb076793334c1ad8d9927ecd9c678d37f4deb2a0
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-11-12 16:06:55 -0300

    Tornando o cors mais seguro

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // habilita CORS globalmente
+  app.enableCors({
+  origin: ['http://localhost:3000'], // só permite esse domínio
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+});
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
